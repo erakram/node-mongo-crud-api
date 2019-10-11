f1fdb103be7a0ccbfb003482cc8158c539a1d761
erakram <aakram@gmail.com>
2019-10-11 17:02:03 +0530

    fixs

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/crudapi'
+    url: 'mongodb://heroku_gsk26bg6:v832rn1m7bvh0rf9valemg9lf9@ds233268.mlab.com:33268/heroku_gsk26bg6'
+    // url: 'mongodb://localhost:27017/crudapi'
 }
