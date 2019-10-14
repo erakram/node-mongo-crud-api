12f9c070fcb654dd035d548fcfbd742a1fba1541
erakram <aakram@gmail.com>
2019-10-14 11:54:58 +0530

    Fix

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://nodeapi-mongo-react.herokuapp.com/');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to node js CRUD API"});
+    res.json({"message": "Welcome to node js CRUD API....."});
 });
 
 // Configuring the database
