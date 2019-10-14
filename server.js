865b1ee9dbad533e7eb9e41ad116b549bf9eba6d
erakram <aakram@gmail.com>
2019-10-14 11:45:29 +0530

    Fix

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://react-api.herokuapp.com/admin');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to node js CRUD API!"});
+    res.json({"message": "Welcome to node js CRUD API"});
 });
 
 // Configuring the database
