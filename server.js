884b86fda27cba6eba34e5ccb153ab5e95af2214
erakram <aakram@gmail.com>
2019-10-14 13:26:46 +0530

    Fix

--- server.js
+++ server.js
@@ -11,8 +11,12 @@ app.use(bodyParser.json())
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://react-api.herokuapp.com/');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    // res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
     console.log(res.header);
+    // console.log(res.header.Access-Control-Allow-Origin);
+
     next();
     
 });
